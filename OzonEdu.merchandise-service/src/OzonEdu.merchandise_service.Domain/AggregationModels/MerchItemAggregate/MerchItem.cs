0af230c3e48fd97cc9fa00618cd981b033c78900
Е.Еникеев <megamannn@mail.ru>
2021-10-31 16:54:33 +0300

    Написан ряд тестов для  MetchItem

--- OzonEdu.merchandise-service/src/OzonEdu.merchandise_service.Domain/AggregationModels/MerchItemAggregate/MerchItem.cs
+++ OzonEdu.merchandise-service/src/OzonEdu.merchandise_service.Domain/AggregationModels/MerchItemAggregate/MerchItem.cs
@@ -13,13 +13,19 @@ namespace OzonEdu.merchandise_service.Domain.AggregationModels.MerchItemAggregat
         public Name Name { get; }
         public ItemEntity Type { get; }
         public ClothingSize ClothingSize { get; }
-        public Quantity Quantity { get; }
+        public Quantity Quantity { get; private set; }
         public MinimalQuantity MinimalQuantity { get; }
-        public string Tag { get; }
+        public Tag Tag { get; }
 
         #endregion
 
-        public MerchItem(Sku sku, Name name, ItemEntity type, ClothingSize clothingSize, Quantity quantity, MinimalQuantity minimalQuantity, string tag)
+        public MerchItem(Sku sku, 
+            Name name, 
+            ItemEntity type, 
+            ClothingSize clothingSize, 
+            Quantity quantity, 
+            MinimalQuantity minimalQuantity, 
+            Tag tag)
         {
             Sku = ValidateSku(sku);
             Name = ValidateName(name);
@@ -27,76 +33,63 @@ namespace OzonEdu.merchandise_service.Domain.AggregationModels.MerchItemAggregat
             ClothingSize = ValidateClothingSize(clothingSize);
             Quantity = ValidateQuantity(quantity);
             MinimalQuantity = ValidateMinimalQuantity(minimalQuantity);
-            Tag = tag;
+            Tag = ValidateTag(tag);
         }
 
         #region Methods
 
-        /// <summary> Проверяет валидность sku </summary>
-        /// <param name="sku"></param>
-        /// <returns></returns>
-        /// <exception cref="ArgumentNullException"></exception>
+        public void IncreaseQuantity(int valueToIncrease)
+        {
+            if (valueToIncrease < 0) throw new ArgumentException("Value to increase cannot be less than zero");
+            Quantity = new Quantity(this.Quantity.Value + valueToIncrease);
+        }
+        
         Sku ValidateSku(Sku sku)
         {
-            if (sku == null) throw new ArgumentNullException("sku cannot be null");
+            if (sku == null) throw new ArgumentNullException("Sku cannot be null");
             return sku;
         }
 
-        /// <summary> Проверяет валидность name </summary>
-        /// <param name="name"></param>
-        /// <returns></returns>
-        /// <exception cref="ArgumentNullException"></exception>
         Name ValidateName(Name name)
         {
-            if (name == null) throw new ArgumentNullException("name cannot be null");
+            if (name == null) throw new ArgumentNullException("Name cannot be null");
+            if (name.Value == "") throw new ArgumentException("Name cannot be empty");
             return name;
         }
 
-        /// <summary> Проверяет валидность item type </summary>
-        /// <param name="itemType"></param>
-        /// <returns></returns>
-        /// <exception cref="ArgumentNullException"></exception>
         ItemEntity ValidateItemType(ItemEntity itemType)
         {
-            if (itemType == null) throw new ArgumentNullException("item type cannot be null");
+            if (itemType == null || itemType.ItemType == null) throw new ArgumentNullException("Item type cannot be null");
             return itemType;
         }
 
-        /// <summary> Проверяет валидность clothing size </summary>
-        /// <param name="clothingSize"></param>
-        /// <returns></returns>
         ClothingSize ValidateClothingSize(ClothingSize clothingSize)
         {
             return clothingSize;
         }
 
-        /// <summary> Проверяет валидность quantity </summary>
-        /// <param name="quantity"></param>
-        /// <returns></returns>
-        /// <exception cref="ArgumentNullException"></exception>
-        /// <exception cref="ArgumentException"></exception>
         Quantity ValidateQuantity(Quantity quantity)
         {
-            if (quantity == null) throw new ArgumentNullException("quantity type cannot be null");
-            if (quantity.Value < 0) throw new ArgumentException("quantity cannot be less than zero");
+            if (quantity == null) throw new ArgumentNullException("Quantity type cannot be null");
+            if (quantity.Value < 0) throw new ArgumentException("Quantity cannot be less than zero");
             return quantity;
         }
 
-        /// <summary> Проверяет валидность minimal quantity </summary>
-        /// <param name="minimalQuantity"></param>
-        /// <returns></returns>
-        /// <exception cref="ArgumentException"></exception>
         MinimalQuantity ValidateMinimalQuantity(MinimalQuantity minimalQuantity)
         {
-            if (minimalQuantity.Value < 0) throw new ArgumentException("quantity cannot be less than zero");
+            if (minimalQuantity.Value < 0) throw new ArgumentException("Quantity cannot be less than zero");
             return minimalQuantity;
         }
-        
-        
+
+        Tag ValidateTag(Tag tag)
+        {
+            return tag;
+        }
 
 
         #endregion
-        
+
+
         
     }
 }
