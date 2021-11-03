8505a15fb46e8cc0a13c07106925d02ceb6b11f7
Е.Еникеев <megamannn@mail.ru>
2021-11-03 21:13:34 +0300

    Доделал Grpc клиента

--- OzonEdu.merchandise-service/src/OzonEdu.MerchandiseService.GrpcClient/SomeGrpcClient.cs
+++ OzonEdu.merchandise-service/src/OzonEdu.MerchandiseService.GrpcClient/SomeGrpcClient.cs
@@ -16,27 +16,41 @@ namespace OzonEdu.MerchandiseService.GrpcClient
 
             try
             {
-
-                for (int i = 1; i < 4; i++)
+                for (int i = 4; i < 7; i++)
                 {
+                    var result = await client.GetMerchIsIssuedAsync(
+                        new GetMerchItemIsGivenRequest() { EmployeeId = i, MerchId = 10 }, 
+                        cancellationToken: CancellationToken.None);
                     
-                }
-                
-                // запрашиваем все товары и информацию о выдаче
-                /*for (int i = 0; i < 5; i++)
-                {
-                    var item = await client.GetMerchByIdAsync(new GetMerchItemByIdRequest { ItemId = i },
+                    Console.WriteLine($"Проверка, выдан ли мерч сотруднику id={i}...");
+                    
+                    if (result.IsIssued)
+                    {
+                        Console.WriteLine("Мерч выдан, выдача не требуется.");
+                        continue;
+                    }
+                    
+                    Console.WriteLine("Мерч не выдан, мерч выдается...");
+                    
+                    await client.GiveMerchToEmployeeAsync(
+                        new GiveMerchItemRequest() { EmployeeId = i, MerchId = 10, ClothingSizeId = 2 }, 
                         cancellationToken: CancellationToken.None);
-                    if (item.ItemName == null) continue;
-
-                    var result = await client.GetMerchIsIssuedByIdAsync(new GetMerchItemByIdRequest { ItemId = i },
+                    
+                    result = await client.GetMerchIsIssuedAsync(
+                        new GetMerchItemIsGivenRequest() { EmployeeId = i, MerchId = 10 }, 
                         cancellationToken: CancellationToken.None);
-                    if (result.IsIssued == null) continue;
-
-                    Console.WriteLine($"Мерч {item.ItemId}: {item.ItemName}");
-                    Console.WriteLine($"Выдан: {result.IsIssued}");
+                    
+                    if (result.IsIssued)
+                    {
+                        Console.WriteLine("Мерч успешно выдан!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Не удалось выдать мерч");
+                    }
                     Console.WriteLine();
-                }*/
+                }
+
 
             }
             catch (RpcException e)
