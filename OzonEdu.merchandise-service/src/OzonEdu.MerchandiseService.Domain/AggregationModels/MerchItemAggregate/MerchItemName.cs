3de76d4d0ff7e4e9548772a127e4ba5a68ba80fb
Е.Еникеев <megamannn@mail.ru>
2021-11-04 13:05:14 +0300

    Отрефакторил код

--- OzonEdu.merchandise-service/src/OzonEdu.MerchandiseService.Domain/AggregationModels/MerchItemAggregate/MerchItemName.cs
+++ OzonEdu.merchandise-service/src/OzonEdu.MerchandiseService.Domain/AggregationModels/MerchItemAggregate/MerchItemName.cs
@@ -4,11 +4,11 @@ using OzonEdu.MerchandiseService.Domain.Models;
 namespace OzonEdu.MerchandiseService.Domain.AggregationModels.MerchItemAggregate
 {
     /// <summary> Наименование товара </summary>
-    public class Name : ValueObject
+    public class MerchItemName : ValueObject
     {
         public string Value { get; }
 
-        public Name(string value)
+        public MerchItemName(string value)
         {
             Value = value;
         }
