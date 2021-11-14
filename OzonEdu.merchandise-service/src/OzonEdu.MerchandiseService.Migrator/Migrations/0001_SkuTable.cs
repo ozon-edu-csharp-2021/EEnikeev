e98b408926076d5da0d41557c5afa85954bd551c
Е.Еникеев <megamannn@mail.ru>
2021-11-14 14:24:07 +0300

    Добавил проект миграций

---
+++ OzonEdu.merchandise-service/src/OzonEdu.MerchandiseService.Migrator/Migrations/0001_SkuTable.cs
@@ -0,0 +1,18 @@
+using FluentMigrator;
+
+namespace OzonEdu.MerchandiseService.Migrator.Migrations
+{
+    [Migration(1)]
+    public class SkuTable : Migration
+    {
+        public override void Up()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void Down()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ OzonEdu.merchandise-service/src/OzonEdu.MerchandiseService.Migrator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OzonEdu.MerchandiseService.Migrator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
