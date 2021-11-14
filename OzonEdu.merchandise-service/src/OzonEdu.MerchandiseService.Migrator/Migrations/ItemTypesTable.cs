66eb64a39b31842def73587793708083bc8a3191
Е.Еникеев <megamannn@mail.ru>
2021-11-14 21:26:38 +0300

    Добавлены несколько табличек

---
+++ OzonEdu.merchandise-service/src/OzonEdu.MerchandiseService.Migrator/Migrations/ItemTypesTable.cs
@@ -0,0 +1,22 @@
+using FluentMigrator;
+
+namespace OzonEdu.MerchandiseService.Migrator.Migrations
+{
+    [Migration(2)]
+    public class ItemTypesTable:Migration
+    {
+        public override void Up()
+        {
+            Create
+                .Table("item_types")
+                .WithColumn("id").AsInt32().PrimaryKey()
+                .WithColumn("name").AsString().NotNullable();
+        }
+
+        public override void Down()
+        {
+            Delete.Table("item_types");
+        }
+        
+    }
+}
