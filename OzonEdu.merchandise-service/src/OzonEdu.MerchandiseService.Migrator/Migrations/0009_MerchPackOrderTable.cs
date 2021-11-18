81eab9c6cb278650e53778c4210cc44a34668cf9
Е.Еникеев <megamannn@mail.ru>
2021-11-19 00:55:56 +0300

    Маленькая правочка

--- OzonEdu.merchandise-service/src/OzonEdu.MerchandiseService.Migrator/Migrations/0009_MerchPackOrderTable.cs
+++ OzonEdu.merchandise-service/src/OzonEdu.MerchandiseService.Migrator/Migrations/0009_MerchPackOrderTable.cs
@@ -8,7 +8,7 @@ namespace OzonEdu.MerchandiseService.Migrator.Migrations
         public override void Up()
         {
             Create.Table("merch_pack_orders")
-                .WithColumn("id").AsInt64().Identity().PrimaryKey()
+                //.WithColumn("id").AsInt64().Identity().PrimaryKey()
                 .WithColumn("employee_id").AsInt64().NotNullable()
                 .WithColumn("clothing_size_id").AsInt32().Nullable()
                 .WithColumn("merch_pack_id").AsInt32().Nullable()
